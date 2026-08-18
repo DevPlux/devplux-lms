1b8a6dee49d551464711aa5df3085d7f9dcc2229
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-08-18 19:33:29 +0530

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { PrismaModule } from './database/prisma/prisma.module';
+import { HealthModule } from './modules/health/health.module';
+import { TenantsModule } from './modules/tenants/tenants.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    HealthModule,
+    TenantsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
