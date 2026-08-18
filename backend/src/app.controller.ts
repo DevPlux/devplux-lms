e3be30fd5c5fc290c79b159b3dbefbea6122ac5f
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-08-18 19:33:37 +0530

    Update app.controller.ts

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
 import { AppService } from './app.service';
+import { TestDto } from './dto/test.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('test-validation')
+  testValidation(@Body() body: TestDto) {
+    return {
+      message: 'Validation successful',
+      data: body,
+    };
   }
 }
