02eea2b5c4be71b6ff9d6dc48ce157cda96192f2
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-08-22 10:25:53 +0530

    Update memberships.controller.ts

--- backend/src/modules/memberships/memberships.controller.ts
+++ backend/src/modules/memberships/memberships.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { CreateMembershipDto } from './dto/create-membership.dto';
+import { MembershipsService } from './memberships.service';
 
 @Controller('memberships')
-export class MembershipsController {}
+export class MembershipsController {
+  constructor(private readonly membershipsService: MembershipsService) {}
+
+  @Post()
+  create(@Body() createMembershipDto: CreateMembershipDto) {
+    return this.membershipsService.create(createMembershipDto);
+  }
+}
