790b0ccdafabea208b185cb9c68b8f3e13617e5d
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-08-23 09:17:50 +0530

    Update institute.module.ts

--- backend/src/modules/institute/institute.module.ts
+++ backend/src/modules/institute/institute.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { InstituteController } from './institute.controller';
 import { InstituteService } from './institute.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [InstituteController],
   providers: [InstituteService],
 })
