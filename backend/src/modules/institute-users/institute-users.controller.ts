be56ff45b7c4035d3f4b344811551031002ba4b9
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-08-23 09:17:17 +0530

    Create institute-users.controller.ts

---
+++ backend/src/modules/institute-users/institute-users.controller.ts
@@ -0,0 +1,55 @@
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+
+import { CurrentTenant } from '../../common/decorators/current-tenant.decorator';
+import { Protected } from '../../common/decorators/protected.decorator';
+import type { TenantRequest } from '../../common/middleware/tenant-resolver.middleware';
+import { CreateInstituteUserDto } from './dto/create-institute-user.dto';
+import { InstituteRole } from '../../generated/prisma/enums';
+
+import { InstituteUsersService } from './institute-users.service';
+import { UpdateInstituteUserRoleDto } from './dto/update-institute-user-role.dto';
+
+type CurrentTenantData = NonNullable<TenantRequest['tenant']>;
+
+@Controller('institute-users')
+export class InstituteUsersController {
+  constructor(private readonly instituteUsersService: InstituteUsersService) {}
+
+  @Get()
+  @Protected(InstituteRole.INSTITUTE_ADMIN)
+  findAll(@CurrentTenant() tenant: CurrentTenantData) {
+    return this.instituteUsersService.findAll(tenant.id);
+  }
+
+  @Get(':userId')
+  @Protected(InstituteRole.INSTITUTE_ADMIN)
+  findOne(
+    @CurrentTenant() tenant: CurrentTenantData,
+    @Param('userId') userId: string,
+  ) {
+    return this.instituteUsersService.findOne(tenant.id, userId);
+  }
+
+  @Post()
+  @Protected(InstituteRole.INSTITUTE_ADMIN)
+  create(
+    @CurrentTenant() tenant: CurrentTenantData,
+    @Body() createInstituteUserDto: CreateInstituteUserDto,
+  ) {
+    return this.instituteUsersService.create(tenant.id, createInstituteUserDto);
+  }
+
+  @Patch(':userId/role')
+  @Protected(InstituteRole.INSTITUTE_ADMIN)
+  updateRole(
+    @CurrentTenant() tenant: CurrentTenantData,
+    @Param('userId') userId: string,
+    @Body() updateRoleDto: UpdateInstituteUserRoleDto,
+  ) {
+    return this.instituteUsersService.updateRole(
+      tenant.id,
+      userId,
+      updateRoleDto.role,
+    );
+  }
+}
