71fa1147a5baddeeedc830dab035f04cfb5d1a6f
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-08-25 12:09:46 +0530

    Update institute-users.module.ts

--- backend/src/modules/institute-users/institute-users.module.ts
+++ backend/src/modules/institute-users/institute-users.module.ts
@@ -4,9 +4,10 @@ import { AuthModule } from '../auth/auth.module';
 
 import { InstituteUsersController } from './institute-users.controller';
 import { InstituteUsersService } from './institute-users.service';
+import { AuditLogsModule } from '../audit-logs/audit-logs.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AuditLogsModule],
   controllers: [InstituteUsersController],
   providers: [InstituteUsersService],
 })
