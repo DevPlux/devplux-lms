e30090af437c0c72f68edcadc9957f97e45a93bd
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-08-27 17:18:20 +0530

    Update auth.module.ts

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,23 +1,35 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import type { StringValue } from 'ms';
-
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+
+import { MailModule } from '../mail/mail.module';
+
 import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { TenantMembershipGuard } from '../../common/guards/tenant-membership.guard';
 import { TenantContextGuard } from '../../common/guards/tenant-context.guard';
 
+import type { StringValue } from 'ms';
+import { AuditLogsModule } from '../audit-logs/audit-logs.module';
+
 @Module({
   imports: [
+    ConfigModule,
+
+    MailModule,
+
+    forwardRef(() => AuditLogsModule),
+
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
+
       useFactory: (configService: ConfigService) => ({
         secret: configService.getOrThrow<string>('JWT_ACCESS_SECRET'),
+
         signOptions: {
           expiresIn: configService.getOrThrow<string>(
             'JWT_ACCESS_EXPIRES_IN',
@@ -26,7 +38,9 @@ import { TenantContextGuard } from '../../common/guards/tenant-context.guard';
       }),
     }),
   ],
+
   controllers: [AuthController],
+
   providers: [
     AuthService,
     JwtAuthGuard,
@@ -34,6 +48,7 @@ import { TenantContextGuard } from '../../common/guards/tenant-context.guard';
     TenantMembershipGuard,
     TenantContextGuard,
   ],
+
   exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
