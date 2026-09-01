90b0f85159ea6e7986be2a8ea71319bbaad5f43d
Janindu Chamod <janiduchamod2001425@gmail.com>
2026-09-01 09:09:14 +0530

    Update nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,18 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2026-08-27",
+
+  devtools: {
+    enabled: true,
+  },
+
+  modules: ["@nuxtjs/tailwindcss"],
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl:
+        process.env.NUXT_PUBLIC_API_BASE_URL ?? "http://localhost:4000/api/v1",
+
+      tenantHost: process.env.NUXT_PUBLIC_TENANT_HOST ?? "",
+    },
+  },
+});
